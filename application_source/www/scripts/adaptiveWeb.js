2b17ccddead881f20535c78e75bef33c66fe230f
LAVRIK596 <Lavrik596@mail.ru>
2025-02-24 14:06:12 +0300

    Декомпозиция основного окна (с выбором других окон) от всех остальных

--- application_source/www/scripts/adaptiveWeb.js
+++ application_source/www/scripts/adaptiveWeb.js
@@ -1,35 +1,48 @@
-const selectCurRobotType = document.getElementById("selectCurRobotType");
+//const selectCurRobotType = document.getElementById("selectCurRobotType");
+const selectScreen = document.getElementById("screenSelector");
 
 const robot_t = {
     trionix: "trionix",
     guppy: "guppy",
-    bag: "bag"
+    bag: "bag",
 };
 const allRobotArray = Object.values(robot_t);
 
-const elementsForChange = {
-    "main_screen": allRobotArray,
-    "mission_screen": [robot_t.guppy],
-    "settings_screen": allRobotArray,
-    "map_screen": allRobotArray,
-    "config_screen": allRobotArray
+const dependsOnRobotType = {
+    "control_select": allRobotArray,
+    "mission_select": [robot_t.guppy],
+    "settings_select": allRobotArray,
+    "map_select": allRobotArray,
+    "config_select": allRobotArray,
 };
 
-function reDisplayELements(dictElement, robotType) {
-    for (var elementId in dictElement) {
-        var curElement = document.getElementById(elementId);
+const screenButtonPairs = {
+    "control_screen": ["control_select"],
+    "map_screen": ["map_select"],
+    "config_screen": ["config_select"],
+};
+
+function reDisplayELements(dictElement, targetElement) {
+    for (let elementId in dictElement) {
+        let curElement = document.getElementById(elementId);
 
         if (curElement == undefined) {
-            console.error(`elementId '${elementId}' is undefined, but it should not be undefined.`)
+            console.error(`elementId "${elementId}" is undefined, but it should not be undefined.`)
             continue;
         }
 
-        curElement.style.display = dictElement[elementId].includes(robotType) ? "block" : "none";
+        curElement.style.display = dictElement[elementId].includes(targetElement) ? "block" : "none";
     }
 }
 
-reDisplayELements(elementsForChange, selectCurRobotType.value);
+reDisplayELements(dependsOnRobotType, robot_t.trionix);
+reDisplayELements(screenButtonPairs, "control_select");
 
-selectCurRobotType.addEventListener("change", () => {
+/*selectCurRobotType.addEventListener("change", () => {
     reDisplayELements(elementsForChange, selectCurRobotType.value);
+});*/
+
+selectScreen.addEventListener("click", (event) => {
+    reDisplayELements(screenButtonPairs, event.target.id);
+    document.getElementById("hamburger_toggle").checked = false;
 });
